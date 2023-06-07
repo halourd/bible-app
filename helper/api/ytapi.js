2cd102f53853ec975a56875a327680f7de07d628
Harold Gallego <halord.py@gmail.com>
2023-06-07 08:26:37 +0800

    use saved api response

--- helper/api/ytapi.js
+++ helper/api/ytapi.js
@@ -7,10 +7,13 @@ const get_latest_videos = async (API_KEY, CHANNEL_ID) => {
         // const response = await axios.get(
         //     `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&maxResults=10&order=date&type=video&key=${API_KEY}`
         // )
+        console.log('RESPONSE: ', response)
 
+        // variable for LIVE API response
+        // let fetched_videos = response.data.items
+        
         let fetched_videos = response
-
-        return fetched_videos
+        return fetched_videos   
 
     }catch(err){
         console.log('[ERROR]',err)

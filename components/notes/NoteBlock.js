c9bf22388fb778f067f273eb0b39a0d2371f389b
Harold Galego <haroldgallego@student.laverdad.edu.ph>
2023-06-11 11:05:44 +0800

    add NoteBlock component

---
+++ components/notes/NoteBlock.js
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import styles from '../../styles/SNotes';
+
+export default class NoteBlock extends Component {
+    render() {
+        return (
+            <View>
+                <TouchableOpacity
+                    activeOpacity={0.6}
+                    onPress={() => {
+                        alert('Functionality not yet implemented.');
+                    }}
+                    style={styles.headerNotes}>
+                    <View>
+                        <View style={styles.notesContainer}>
+                            <Text style={styles.notesTitle}>Thanksgiving</Text>
+                            <Text style={styles.notesDate}>12/13</Text>
+                        </View>
+                        <View style={styles.notesContainer1}>
+                            <View style={styles.verses}>
+                                <Text style={styles.noteverses}>1Cor 1:1</Text>
+                                <Text style={styles.noteverses}>1Cor 1:2</Text>
+                                <Text style={styles.noteverses}>1Cor 1:3</Text>
+                            </View>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
--- screens/Notes.js
+++ screens/Notes.js
@@ -3,6 +3,7 @@ import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import NavigationBar from '../components/NavigationBar';
 
+import NoteBlock from '../components/notes/NoteBlock';
 import styles from '../styles/SNotes';
 import { ScrollView } from 'react-native-gesture-handler';
 
@@ -47,111 +48,7 @@ export default class App extends Component {
             </View>
           </View>
         </TouchableOpacity>
-
-        <TouchableOpacity 
-        activeOpacity={0.6}
-        onPress={() => {
-          alert('Functionality not yet implemented.');
-        }}
-        style={styles.headerNotes}>
-          <View>
-            <View style={styles.notesContainer}>
-              <Text style={styles.notesTitle}>Thanksgiving</Text>
-              <Text style={styles.notesDate}>12/13</Text>
-            </View>
-            <View style={styles.notesContainer1}>
-              <View style={styles.verses}>
-                <Text style={styles.noteverses}>1Cor 1:1</Text>
-                <Text style={styles.noteverses}>1Cor 1:2</Text>
-                <Text style={styles.noteverses}>1Cor 1:3</Text>
-              </View>
-            </View>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-        activeOpacity={0.6}
-        onPress={() => {
-          alert('Functionality not yet implemented.');
-        }}
-        style={styles.headerNotes}>
-          <View>
-            <View style={styles.notesContainer}>
-              <Text style={styles.notesTitle}>My Notes 01-15-23</Text>
-              <Text style={styles.notesDate}>12/13</Text>
-            </View>
-            <View style={styles.notesContainer1}>
-              <View style={styles.verses}>
-                <Text style={styles.noteverses}>1Cor 1:1</Text>
-                <Text style={styles.noteverses}>1Cor 1:2</Text>
-                <Text style={styles.noteverses}>1Cor 1:3</Text>
-              </View>
-            </View>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-        activeOpacity={0.6}
-        onPress={() => {
-          alert('Functionality not yet implemented.');
-        }}
-        style={styles.headerNotes}>
-          <View>
-            <View style={styles.notesContainer}>
-              <Text style={styles.notesTitle}>Worship Service</Text>
-              <Text style={styles.notesDate}>12/13</Text>
-            </View>
-            <View style={styles.notesContainer1}>
-              <View style={styles.verses}>
-                <Text style={styles.noteverses}>1Cor 1:1</Text>
-                <Text style={styles.noteverses}>1Cor 1:2</Text>
-                <Text style={styles.noteverses}>1Cor 1:3</Text>
-              </View>
-            </View>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-        activeOpacity={0.6}
-        onPress={() => {
-          alert('Functionality not yet implemented.');
-        }}
-        style={styles.headerNotes}>
-          <View>
-            <View style={styles.notesContainer}>
-              <Text style={styles.notesTitle}>PM 06-23-23</Text>
-              <Text style={styles.notesDate}>12/13</Text>
-            </View>
-            <View style={styles.notesContainer1}>
-              <View style={styles.verses}>
-                <Text style={styles.noteverses}>1Cor 1:1</Text>
-                <Text style={styles.noteverses}>1Cor 1:2</Text>
-                <Text style={styles.noteverses}>1Cor 1:3</Text>
-              </View>
-            </View>
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-        activeOpacity={0.6}
-        onPress={() => {
-          alert('Functionality not yet implemented.');
-        }}
-        style={styles.headerNotes}>
-          <View>
-            <View style={styles.notesContainer}>
-              <Text style={styles.notesTitle}>Prayer Meeting</Text>
-              <Text style={styles.notesDate}>12/13</Text>
-            </View>
-            <View style={styles.notesContainer1}>
-              <View style={styles.verses}>
-                <Text style={styles.noteverses}>1Cor 1:1</Text>
-                <Text style={styles.noteverses}>1Cor 1:2</Text>
-                <Text style={styles.noteverses}>1Cor 1:3</Text>
-              </View>
-            </View>
-          </View>
-        </TouchableOpacity>
+        <NoteBlock />
         </ScrollView>
         <NavigationBar name="Home" navigation={this.props.navigation} />
       </View>

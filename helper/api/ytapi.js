830d897686e32142fb9c4304d7b5edba5a9bdd98
Harold Gallego <haroldgallego@student.laverdad.edu.ph>
2025-02-05 18:07:03 +0800

    Push

--- helper/api/ytapi.js
+++ helper/api/ytapi.js
@@ -4,16 +4,16 @@ import response from '../../config/response';
 const get_latest_videos = async (API_KEY, CHANNEL_ID) => {
     console.log('API: ', API_KEY, 'CHANNEL: ', CHANNEL_ID)
     try{
-        // const response = await axios.get(
-        //     `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&maxResults=10&order=date&type=video&key=${API_KEY}`
-        // )
-        // console.log('RESPONSE: ', response)
+        const response = await axios.get(
+            `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&maxResults=10&order=date&type=video&key=${API_KEY}`
+        )
+        console.log('RESPONSE: ', response)
 
         // variable for LIVE API response
-        // let fetched_videos = response.data.items
+        let fetched_videos = response.data.items
         
-        let fetched_videos = response
-        return fetched_videos   
+        // let fetched_videos = response
+        // return fetched_videos   
 
     }catch(err){
         console.log('[ERROR]',err)
